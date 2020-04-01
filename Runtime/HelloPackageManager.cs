ed09eb0294abbf93328d6a9664258ad8b9049bea
afjk01 <afjk01@gmail.com>
2020-04-01 14:04:02 +0900

    New.

---
+++ Runtime/HelloPackageManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace com.afjk.test
+{
+    public class HelloPackageManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("HelloPackageManager !");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+    
+}
