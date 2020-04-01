d8fd6b6881f67ca934a40851b41c1844f8d31bb9
afjk01 <afjk01@gmail.com>
2020-04-01 16:10:43 +0900

    version up test.

---
+++ Runtime/TestClass2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.afjk.test
+{
+    public class TestClass2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("TestClass2");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
